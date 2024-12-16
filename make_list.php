05e9a7210637aca7516fcdc8aa49a1ba7f0ff211
Karel Wintersky <wombat@fontanka.ru>
2024-12-17 02:41:41 +0300

    1.1.2
    * calc list script

--- make_list.php
+++ make_list.php
@@ -103,7 +103,7 @@ function shuffle_forced(array $array):array {
 
 $json = file_get_contents(__DIR__ . '/members.json');
 
-$recipients = [];
+$recipients_list = [];
 
 $senders = [];
 $max_cards = 0;
@@ -115,54 +115,111 @@ foreach (json_decode($json, true) as $s) {
 
     $senders[] = ['will_recieve' => 0, 'will_send' => $s['cards_count'], ...$s ];
 
-    $will_sender[] = [
-        'id'        =>  $s['id'],
-        'from'      =>  $s['fio'],
-        'email'     =>  $s['email'],
-        'letters'   =>  []
-    ];
-
-    $recipients[] = [
+    $senders_list[] = [
         'id'        =>  $s['id'],
         'fio'       =>  $s['fio'],
         'email'     =>  $s['email'],
         'letters'   =>  []
     ];
+    $recipients_list[ $s['email'] ] = 0;
+
+    /*$recipients_list[] = [
+        'id'        =>  $s['id'],
+        'fio'       =>  $s['fio'],
+        'email'     =>  $s['email'],
+        'letters'   =>  []
+    ];*/
 
 }
 
-do {
-    // каждая итерация это шаффл, потом мерж массива
-    $possible_recipients = shuffle_forced($senders);
+while (true) {
+    $total_send_letters = 0;
 
-    $senders_count = 0;
-    foreach ($senders as $i => $sender) {
-        if ($sender['will_send'] > 0) {
-            $sender_real_id = $sender['id'];
-            arrDec($senders, 'id', $sender_real_id, 'will_send'); // уменьшаем число писем на 1 у этого отправителя
+    // clean datasets
+    foreach ($senders_list as $i => $s) {
+        $senders_list[$i]['letters'] = [];
+    }
+    foreach ($recipients_list as $i => $r) {
+        $recipients_list[$i] = 0;
+    }
 
-            $recipient = $possible_recipients[$i];
-            $recipient_real_id = $recipient['id'];
+    // iterate each sender
+    foreach ($senders as $sender) {
+        $possible_recipients = array_filter($senders, function ($item) use ($sender){
+            return $item['will_recieve'] < 3;
+        });
 
-            arrPut($recipients, 'id', $sender_real_id, 'letters', [
+        $possible_recipients = array_filter($possible_recipients, function ($item) use ($sender){
+            return $item['id'] !== $sender['id'];
+        });
+
+        $possible_recipients = shuffle_forced($senders);
+        $possible_recipients = array_slice($possible_recipients, 0, $sender['will_send']);
+
+        foreach ($possible_recipients as $recipient) {
+            arrInc($senders, 'id', $recipient['id'], 'will_recieve');
+            arrPut($senders_list, 'id', $sender['id'], 'letters', [
                 'id'        =>  $recipient['id'],
+                'email'     =>  $recipient['email'],
                 'fio'       =>  $recipient['fio'],
                 'address'   =>  $recipient['address']
             ]);
 
-            $senders_count++;
+            // $recipients_list[$recipient['email']] = ($recipients_list[$recipient['email']] ?? 0) + 1; // by AI
+            $recipients_list[$recipient['email']]++;
+
+            $total_send_letters++;
         }
     }
-    if (empty($senders_count)) {
+
+    $all_recieved_letters = true;
+    foreach ($recipients_list as $recieved_count) {
+        $all_recieved_letters = $all_recieved_letters && (bool)$recieved_count;
+    }
+    if ($all_recieved_letters) {
         break;
     }
+}
 
-} while(true);
+sort($senders_list);
 
-sort($will_sender);
-sort($recipients);
+$bottom_message = [
+    'автоматический распределитель писем',
+    'тайный письмомиксер',
+    'волшебный шляп (м.р. от шляпа)',
+    'генератор неслучайных чисел',
+    'ручной слоувомбат',
+    'неосязаемый почтальон сновидений',
+    'сотрудник техотдела ООО Психотроника'
+];
 
-var_dump($recipients);
+$message = '';
+foreach ($senders_list as $sender) {
+
+    $waiter = count($sender['letters']) > 1 ? 'ваших открыток очень ждут' : 'вашу открытку очень ждёт';
+    $message .= <<<HEAD
+----------------------------------------------------------------------
+Кому: {$sender['email']}
+----
+Уважаемый(ая) {$sender['fio']}, {$waiter}:\n\n
+HEAD;
+    foreach ($sender['letters'] as $letter) {
+        $message .= <<<LETTER
+* {$letter['fio']}, по адресу: {$letter['address']}\n
+LETTER;
+    }
+    $who = $bottom_message[ mt_rand(0, count($bottom_message))-1 ];
+    $message .= <<<BOTTOM
+
+С уважением, ваш покорный слуга, {$who}.\n  
+BOTTOM;
+
+}
+
+var_dump($senders_list);
+var_dump($message);
+var_dump($recipients_list);
+var_dump($total_send_letters);
 
 
 
